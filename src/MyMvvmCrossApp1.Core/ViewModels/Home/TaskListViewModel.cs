393cb0872490ebab60267e0e810641c1fbbf70da
Boris <Boris_prog@DESKTOP-7MF7U6S>
2022-02-19 11:01:23 +0500

    Add Type Compatibility

--- src/MyMvvmCrossApp1.Core/ViewModels/Home/TaskListViewModel.cs
+++ src/MyMvvmCrossApp1.Core/ViewModels/Home/TaskListViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using MvvmCross.ViewModels;
 using MyMvvmCrossApp1.Core.Services;
 
@@ -13,13 +14,18 @@ namespace MyMvvmCrossApp1.Core.ViewModels.Home
             _taskListService = taskListService;
         }
 
-        public List<string> TaskNames
+        public Task<List<string>> TaskNames
         {
             get => _taskNames;
             set => SetProperty(ref _taskNames, value);
         }
+        //public Task<List<string>> ShowTasks()
+        //{
+        //    TaskNames = _taskListService.ShowTaskList();
+        //    return TaskNames;
+        //}
 
-        private List<string> _taskNames;
+        private Task<List<string>> _taskNames;
         private readonly TaskListService _taskListService;
     }
 }
